430a0656ee4cc49635445951729514824260ecb2
EnterpriseJavaDeveloper <109693437+EnterpriseJavaDeveloper@users.noreply.github.com>
2023-01-10 15:52:09 -0700

    1 file modified, 2 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
+import Navbar from "./components/Navbar"
+import Main from "./components/Main"
 import "./style.css";
 
 export default function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <div className="container">
+    <Navbar />
+    <Main />
+</div>
   );
 }
