0a36785b511d64da36185fa2412115721c7a19d0
EnterpriseJavaDeveloper <109693437+EnterpriseJavaDeveloper@users.noreply.github.com>
2023-01-10 16:46:44 -0700

    3 files modified, 1 added, 1 deleted

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,9 @@
 import React from "react"
 
 export default function Navbar() {
-    return (<div>
-              <h1>ReactFacts</h1>
-              <h1>Rect Course - Project 1</h1>
-            </div>)
+    return (<nav className="headerBar">
+                <img src="https://raw.githubusercontent.com/EnterpriseJavaDeveloper/porfolio/main/src/reactjs-icon.png" className="nav--icon"/>
+                <h3 className="nav--logo_text">ReactFacts</h3>
+              <h4 className="nav--title">React Course - Project 1</h4>
+            </nav>)
 }
