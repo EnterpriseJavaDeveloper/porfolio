c68093c03673457788db5622bdf913421b336558
EnterpriseJavaDeveloper <109693437+EnterpriseJavaDeveloper@users.noreply.github.com>
2023-01-10 17:10:30 -0700

    2 files modified

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,5 +1,14 @@
 import React from "react"
 
 export default function Main() {
-  return <h1>Main component</h1>
+  return <div className="content--body">
+            <h1 className="content--title">Fun Facts about React</h1>
+            <ul className="content--list">
+              <li>Was first released in 2013</li>
+              <li>Was originally created by Jordan Walke</li>
+              <li>Has well over 100K stars on GitHub</li>
+              <li>Is maintained by Facebook</li>
+              <li>Powers thousands of enterprise apps, including mobile apps</li>
+            </ul>
+          </div>
 }
